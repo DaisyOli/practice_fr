d9f066bc2a8d1cc550c8622092796dad0dfeda53
daisy oliani <daisy.oliani@gmail.com>
2025-03-26 21:16:17 +0100

    Correção da configuração do Rack::Timeout

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,2 +1,4 @@
 # Define timeout para requisições no Heroku
-Rack::Timeout.timeout = 20 if defined?(Rack::Timeout) # em segundos 
+if defined?(Rack::Timeout)
+  Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 20
+end 
