81a34e6cbcd548df1d05f2fa5b3fee721a470f09
daisy oliani <daisy.oliani@gmail.com>
2025-03-26 21:11:57 +0100

    Ajustando configurações de ambiente para o novo aplicativo Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     ENV["DOMAIN_NAME"], # Domínio principal
-    "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" # Subdomínio do Heroku
+    "practice-francais.herokuapp.com" # Subdomínio do Heroku (atualizado para o novo app)
   ]
   
   # Skip DNS rebinding protection for the default health check endpoint.
